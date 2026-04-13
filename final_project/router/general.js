14e56e94e48d7e9fd2b7ab1290d4ee109cdd86e1
phelistus003 <phelistuskangu93@gmail.com>
2026-04-13 16:24:48 -0400

    Task 2: Get book by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,7 +20,7 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-  res.send(JSON.stringify(books[isbn], null, 4));
+    res.send(JSON.stringify(books[isbn], null, 4));
   //Write your code here
  // return res.status(300).json({message: "Yet to be implemented"});
  });
