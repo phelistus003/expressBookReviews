9bfc028b098a725b6f52aee4a7f06e9cbd940105
phelistus003 <phelistuskangu93@gmail.com>
2026-04-13 17:02:43 -0400

    Task 8: Add/modify book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -15,14 +15,41 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
+
+    const username = req.body.username;
+  const password = req.body.password;
+
+  if(!username || !password) {
+    return res.status(400).json({message: "Username and password are required"});
+  }
+
+  if(authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({ data: password }, 'access', { expiresIn: 60*60 });
+    req.session.authorization = { accessToken, username };
+    return res.status(200).json({message: "User successfully logged in"});
+  } else {
+    return res.status(401).json({message: "Invalid username or password"});
+  }
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
+
+    const isbn = req.params.isbn;
+  const review = req.query.review;
+  const username = req.session.authorization.username;
+
+  if(books[isbn]) {
+    books[isbn].reviews[username] = review;
+    return res.status(200).json({message: `Review for ISBN ${isbn} added/updated successfully`});
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
+
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.authenticated = regd_users;
